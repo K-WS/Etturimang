ee9aba392330b7d4815e9f2a6c7c860223ebd603
K-W S <karl-walter@hotmail.com>
2016-03-09 18:39:19 +0200

    lühikirjeldus mängu eesmärgile + väike bug pärast musta käiku eemaldatud.

--- Grupitöö 1/src/Etturimang.java
+++ Grupitöö 1/src/Etturimang.java
@@ -6,6 +6,16 @@ import java.util.Scanner;
 public class Etturimang {
 
 	public static void main(String[] args) {
+		System.out.println("Etturimäng: ");
+		System.out.println("1.Käigu sisestamine");
+		System.out.println("    Vastavalt sellele, kelle käik on praegu, tuleb kirjutada neljasümboline string");
+		System.out.println("    näiteks A7A6, kus esimesed 2 sümbolit tähistavad malendi esialgset asukohta");
+		System.out.println("    ja viimased 2 asukohta, kuhu soovid liigutada oma etturit.");
+		System.out.println();
+		System.out.println("2.Mängu ülesanne");
+		System.out.println("    jõuda ühe oma etturitega lõpuni välja, või võtta maha kõik vastase etturid.");
+		System.out.println("Kõikide muude reeglite poolest käib see samamoodi nagu tavaline male.");
+		System.out.println();
 		Scanner sc = new Scanner(System.in);
 		int kaik = 1;
 		
@@ -22,36 +32,31 @@ public class Etturimang {
 			//While tsükkel, mis kestab niikaua, kuni mäng saab läbi
 			while(praegune_mang){
 				
-
 				//While tsükkel, mis kestab niikaua, kui mängija pole oma käiku sisestanud.
 				while(kaik == 1){System.out.println("Sisesta käik, valge ");
-				String a = sc.nextLine().toUpperCase();
+					String a = sc.nextLine().toUpperCase();
+					if(a.equals("RESTART")){praegune_mang = false;break;}
 				
-				if(a.equals("RESTART")){praegune_mang = false;break;}
-				
-				boolean b = ref.Assess(a);
-				if (b==true){
-					malelaud.liigutus(a);
-					kaik = 0;
-					ref.nextTurn();
-
-				}
+					boolean b = ref.Assess(a);
+					if (b==true){
+						malelaud.liigutus(a);
+						kaik = 0;
+						ref.nextTurn();
+					}
 				}
 				//Teine While tsükkel, mis kestab niikaua, kui mängija pole oma käiku sisestanud.
 				while(kaik == 0){System.out.println("Sisesta käik, must ");
-				String a = sc.nextLine().toUpperCase();
-				boolean b = ref.Assess(a);
-				if (b==true){
-					malelaud.liigutus(a);
-					kaik = 1;
-					ref.nextTurn();
-
-				}
+					String a = sc.nextLine().toUpperCase();
+					
+					boolean b = ref.Assess(a);
+					if (b==true){
+						malelaud.liigutus(a);
+						kaik = 1;
+						ref.nextTurn();
+					}
 				praegune_mang = !ref.gameOver();
 				
 				}
-				
-				System.out.println(malelaud);
 			}
 			System.out.println("Valge lõpp-punktid: "+valge.getPoints()+", Musta lõpp-punktid: "+must.getPoints());
 			System.out.println("Kas soovid edasi mängida? (jah/ei)");
@@ -63,7 +68,6 @@ public class Etturimang {
 					malelaud = new Malelaud();
 					praegune_mang = true;
 				}
-				
 				else if(arg.equals("ei")){onVastus = false; kas_mangid_edasi = false;}}
 		}		
 	}
