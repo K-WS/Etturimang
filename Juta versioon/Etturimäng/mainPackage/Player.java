25713bd2af1aeeada50db0e6ff629287d5a83d16
Yootah <juta.mae13@gmail.com>
2016-03-17 17:53:13 +0200

    kommentaarid

--- Juta versioon/Etturimäng/mainPackage/Player.java
+++ Juta versioon/Etturimäng/mainPackage/Player.java
@@ -1,38 +1,75 @@
 package mainPackage;
 
 
+/**
+ * Klass, mis hoiab endas mängija andmeid. 
+ */
 public class Player {
 	
+	/** Mitu etturit on mängijal järel. */
 	private int pawnsLeft;
+		
+		/**
+		 * Gets the pawns left.
+		 *
+		 * @return the pawns left
+		 */
 		public int getPawnsLeft() 
 		{ 
 			return pawnsLeft; 
 		}
+		
+		/**
+		 * Pawn killed - käivitatakse, kui vastane võtab mõne etturi maha.  
+		 */
 		public void pawnKilled() 
 		{
 			pawnsLeft--;
 		}
+		
+		/**
+		 * Reset pawns left - käivitatakse uue mängu alustamisel. 
+		 */
 		public void resetPawnsLeft()
 		{
 			pawnsLeft = 8;
 		}
 		
+	/** The points - mitu mängu on võidetud. Viikide eest meie programm punkte ei anna. */
 	private int points;
+		
+		/**
+		 * Gets the points.
+		 *
+		 * @return the points
+		 */
 		public int getPoints() 
 		{ 
 			return points; 
 		}
+		
+		/**
+		 * Won the match - käivitatakse, kui mõni ettur on jõudnud laua serva või kui kõik vastase etturid on hävitatud.
+		 */
 		public void wonTheMatch()
 		{
 			points++;
 		}
 	
+	/**
+	 * Instantiates a new player.
+	 */
 	public Player() 
 	{
 		pawnsLeft = 8;
 		points = 0;
 	}
 
+	/**
+	 * Hack pawns left - meetod, mida kasutatakse mängu eri seisude kiireks testimiseks. 
+	 *
+	 * @param i - mitu etturit on mängijale kunstlikult tekitatud
+	 */
 	public void hackPawnsLeft(int i)
 	{
 		while(pawnsLeft>i)
--- Juta versioon/Etturimäng/unitTests/Referee_GenericInputTest.java
+++ Juta versioon/Etturimäng/unitTests/Referee_GenericInputTest.java
@@ -4,6 +4,7 @@ import mainPackage.Malelaud;
 import mainPackage.Player;
 
 public class Referee_GenericInputTest {
+	//Klass, kus saab testida vigaseid sisendeid ilma et mäng käiks
 
 	public static void main(String[] args) {
 		Malelaud brd = new Malelaud();
@@ -11,6 +12,11 @@ public class Referee_GenericInputTest {
 		Player b = new Player();
 		
 		Referee ref = new Referee(brd, w, b);
+		
+		//Selleks tuleb kirjutada oma sisend siia (stringi kohale): 
 		System.out.println(ref.Assess("F8F7"));
+		
+		// Väljund on true, kui käik on lubatud. 
+		// Enamikel juhtudel antakse ka verbaalset tagasisidet vea olemuse kohta.
 	}
 }
